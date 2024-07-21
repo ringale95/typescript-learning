5132d39db6fca42159c3be436e8b69d9124b6eb3
Raveena Ingale <ingale.r@northeastern.edu>
2024-07-21 14:15:49 -0400

    feat: new code

---
+++ src/index.ts
@@ -0,0 +1,4 @@
+import * as _ from 'lodash';
+
+let result = _.add(2,3);
+console.log(result);
---
+++ src/tax.js
@@ -0,0 +1,8 @@
+// @ts-nocheck
+export function calculateTax(income){
+    return income * 0.3;
+}
+
+export function sayHello() {
+    console.log('Hello!');
+}
